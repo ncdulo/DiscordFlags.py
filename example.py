3d7e409a6599f5b369ad1bd67588850198140938
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-12 10:27:59 -0400

    Update `example.py` to use new module
    Update of the example code to utilize the new module created in the last
    commit. The core change is in the `import` statement. By using `import
    discordflags`, we tell Python to import the module located in the
    `discordflags` directory. When importing the module, Python
    automatically runs `discordflags/__init__.py` which is what brings in
    the `flagsfromint` function.
    
    The `.. as df` on the end of the `import` statement is basically
    creating an alias, or shorter name, for `discordflags`. This means that
    in the loop below, we will call `df.flagsfromint()` instead of
    `discordflags.flagsfromint`. The inclusion of this `as df` part is
    optional, but allows for less typing with  long module names.

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
-import flags
+import discordflags as df
 
 
 if __name__ == '__main__':
     print('Press Control-C to exit.')
     while True:
         x = input("User flags value: ")
-        if flags.flagsfromint(x):
-            for y in flags.flagsfromint(x):
+        if df.flagsfromint(x):
+            for y in df.flagsfromint(x):
                 print(y)
         else:
             print("Invalid flag value passed.")
