98e5e8248cde56d5e945e7f80190c1dac7c960b5
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-12 10:34:39 -0400

    Exit example.py gracefully
    When exiting the `examply.py` program, there would be a few lines of
    error output from Python. It looks a little bit cleaner if we show a
    proper 'exit' so the user does not misinterpret that as an error.
    
    By adding this `try` block we can `try` to do whatever is written
    inside. This case, is the `input` statement. If there is either a
    `KeyboardInterrupt` (Control-C) or `EOFError` (Control-D), we capture it
    in the `except` block and display the error message before exiting. This
    will only catch the two specific exceptions we listed. This is good
    because other errors will still be displayed.

--- example.py
+++ example.py
@@ -4,7 +4,12 @@ import discordflags as df
 if __name__ == '__main__':
     print('Press Control-C to exit.')
     while True:
-        x = input("User flags value: ")
+        try:
+            x = input("User flags value: ")
+        # Exit cleanly when the user presses 'Control-D' or 'Control-C'
+        except (EOFError, KeyboardInterrupt):
+            print('\nExiting.')
+            exit()
         if df.flagsfromint(x):
             for y in df.flagsfromint(x):
                 print(y)
