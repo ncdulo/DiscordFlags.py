26049f6e256e5a5deb72fb2e96094423140a77cc
panley01 <52224740+panley01@users.noreply.github.com>
2020-04-12 10:58:41 +0100

    Corrected file commit

--- flags.py
+++ flags.py
@@ -1,56 +1,62 @@
 staff = 1
-partner = 2
-hypesquad = 4
-bug_hunter = 8
+partner = 1<<1
+hypesquad = 1<<2
+bug_hunter = 1<<3
 unknown_4 = 1<<4
 unknown_5 = 1<<5
-hypesquad_bravery = 64
-hypesquad_brilliance = 128
-hypesquad_balance = 256
+hypesquad_bravery = 1<<6
+hypesquad_brilliance = 1<<7
+hypesquad_balance = 1<<8
 early_supporter = 1<<9
-team_user = 1024
+team_user = 1<<10
 unknown_11 = 1<<11
-system = 4096
+system = 1<<12
 unknown_13 = 1<<13
 bug_hunter_2 = 1<<14
 unknown_15 = 1<<15
 unknown_16 = 1<<16
-while True:
-    flags=input("flags: ")
+unknown_17 = 1<<17
+#all current flags defined here
+def flagsfromint(flags)
     if flags.isnumeric():
-        print("Valid flags")
         flags = int(flags)
+        flaglist=[]
         if (flags & staff) == staff:
-            print("Staff")
+            flaglist.append("Staff")
         if (flags & partner) == partner:
-            print("Partner")
+            flaglist.append("Partner")
         if (flags & hypesquad) == hypesquad:
-            print("Hypesquad Events")
+            flaglist.append("Hypesquad Events")
         if (flags & bug_hunter) == bug_hunter:
-            print("Bug hunter")
+            flaglist.append("Bug hunter")
         if (flags & unknown_4) == unknown_4:
-            print("unknown_4")
+            flaglist.append("MFA_SMS")
         if (flags & unknown_5) == unknown_5:
-            print("unknown_5")
+            flaglist.append("PREMIUM_PROMO_DISMISSED")
         if (flags & hypesquad_bravery) == hypesquad_bravery:
-            print("Hypesquad bravery")
+            flaglist.append("Hypesquad bravery")
         if (flags & hypesquad_brilliance) == hypesquad_brilliance:
-            print("Hypesquad brilliance")
+            flaglist.append("Hypesquad brilliance")
         if (flags & hypesquad_balance) == hypesquad_balance:
-            print("Hypesquad balance")
+            flaglist.append("Hypesquad balance")
         if (flags & early_supporter) == early_supporter:
-            print("Early nitro")
+            flaglist.append("Early nitro")
         if (flags & team_user) == team_user:
-            print("Team user")
+            flaglist.append("Team user")
         if (flags & unknown_11) == unknown_11:
-            print("unknown_11")
+            flaglist.append("Unused")
         if (flags & system) == system:
-            print("System")
+            flaglist.append("System")
         if (flags & unknown_13) == unknown_13:
-            print("unknown_13")
+            flaglist.append("Unread urgent system message ")
         if (flags & bug_hunter_2) == bug_hunter_2:
-            print("Bug hunter lvl2")
+            flaglist.append("Bug hunter lvl2")
         if (flags & unknown_15) == unknown_15:
-            print("unknown_15")
+            flaglist.append("UNDERAGE_DELETED")
         if (flags & unknown_16) == unknown_16:
-            print("Verified Bot")
+            flaglist.append("Verified Bot")
+        if (flags & unknown_17) == unknown_17:
+            flaglist.append("Verified Developer")
+        return flaglist
+    else:
+        return False
