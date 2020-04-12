95148fdb277f4636f0b4fba7993a89a10d26c35c
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-12 09:55:36 -0400

    Fix indentation error. Use __main__ idiom.
    When initially trying the `example.py` file, it did not work. There was
    an indentation error in the `for` block. I fixed that error by adding
    the missing indentation.
    
    I also placed the example code into an `if __name__ ...` block. This is
    a common idiom used in Python to only run certain code when the file is
    called directly. Code inside this block will not run when you use
    `import discordflags`. This means it is a good place to include the
    actual user interacting part of the code.
    
    You can read more about the `if __name__ ...` main idiom, if you like,
    at this very detailed StackOverflow answer:
    
      - https://stackoverflow.com/questions/419163/what-does-if-name-main-do

--- example.py
+++ example.py
@@ -1,8 +1,12 @@
 import flags
-while True:
-    x = input("User flags value: ")
-    if flags.flagsfromint(x):
-        for y in flags.flagsfromint(x):
-        print(y)
-    else:
-        print("Invalid flag value passed.")
+
+
+if __name__ == '__main__':
+    print('Press Control-C to exit.')
+    while True:
+        x = input("User flags value: ")
+        if flags.flagsfromint(x):
+            for y in flags.flagsfromint(x):
+                print(y)
+        else:
+            print("Invalid flag value passed.")
